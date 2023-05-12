e85e9d3859a7df9ec8df94024bfecd379e3b1a06
Connor0627 <137connor@gmail.com>
2023-05-12 03:07:36 -0700

    2nd commit

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
+from copy import deepcopy
 class Node:
-    def __init__(self, puzzle, g, h):
+    def __init__(self, puzzle, gn, hn):
         self.puzzle = puzzle
-        self.g = g
-        self.h = h
+        self.gn = gn
+        self.hn = hn
+        self.goal = []
+        i = 0
+        j = 0
+        num = 1
+        for i in range(len(self.puzzle)):
+            line = []
+            for j in range(len(self.puzzle[0])):
+                line.append(num)
+                num += 1
+            self.goal.append(line)
+        self.goal[i][j] = 0
 
+    def get_fn(self):
+        return self.gn + self.hn
 
+    def get_blank(self):
+        for i in range(len(self.puzzle)):
+            for j in range(len(self.puzzle[0])):
+                if self.puzzle[i][j] == 0:
+                    return i, j
+
+    def print_puzzle(self):
+        for i in range(len(self.puzzle)):
+            print(self.puzzle[i])
+        print("\n")
+
+    def is_goal(self):
+        if self.puzzle == self.goal:
+            return True
+        return False
+
+def expand(node):
+    kidsnode = []
+    for i in range(0, 4):
+        kid = deepcopy(node)
+        kid.gn += 1
+        moveBlankTile(node.puzzle, kid.puzzle, i)
+
+def moveBlankTile():
+    a
 def general_search(problem, queueing_function):
     nodes = [Node(problem, 0, 0)]  # nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL-STATE))
     while True:
         if len(nodes) == 0:
             return False
         node = nodes.pop();
-        if is_goal(node.puzzle):
+        if node.is_goal():
             return node
-        # nodes = queueing_function(nodes, expand(node))
-
-
+        nodes = queueing_function(nodes, expand(node))
 # check if the current puzzle is goal puzzle
-def is_goal(puzzle):
-    num = 1;
-    i = 0
-    j = 0
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[0])):
-            if puzzle[i][j] == num:
-                num += 1
-            else:
-                break
-    if num == len(puzzle) * len(puzzle[0]) and puzzle[i][j] == 0:
-        return True
-    return False
-
-
-def print_puzzle(puzzle):
-    for i in range(len(puzzle)):
-        print(puzzle[i])
-    print("\n")
 
 
 if __name__ == "__main__":
     p = [[3, 1, 2], [4, 5, 6], [7, 8, 0]]
-    res = general_search(p, "print()")
-    if not res:
-        print("failll")
-    else:
-        print_puzzle(res.puzzle)
+    q = [[3, 1, 2], [4, 5, 6], [7, 8, 0]]
+    n = Node(p, 0 ,0)
+    print(n.goal)
