3f73b373edb4ebb08637443cba13b693f15ec2c4
Connor0627 <137connor@gmail.com>
2023-06-13 00:28:36 -0700

    1st commit

---
+++ Project2.py
@@ -0,0 +1,10 @@
+# A function that returns the length of the value:
+def myFunc(e):
+  return len(e)
+
+cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+
+cars.sort(key=myFunc, reverse = True)
+
+print(cars[0])
+print(cars.pop())
